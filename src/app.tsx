2cde06c3778213c54a961d1df4ac65c1850c744a
KrinjMaster <5opkasuka@gmail.com>
2023-02-18 14:20:41 +0300

    upd

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Product from './Components/Product';
-import axios from 'axios';
-import { IProduct } from './Module';
 
 const App: React.FC = () => {
-  const [products, setProducts] = useState<IProduct[]>([])
-
-  async function getProducts() {
-    const response = await axios.get<IProduct[]>(`https://fakestoreapi.com/products/`)
-    setProducts(response.data)
-  }
-
-  useEffect(() => {
-    getProducts()
-  },[])
-
-
   return (
-    <div>
-      { products.map(product => <Product product={product} key={product.id}/>) }
-    </div>
+    <></>
   )
 }
 
