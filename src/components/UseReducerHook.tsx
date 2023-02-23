c79a27d92e849f5b11f9296d3af0915d6b5a1d54
KrinjMaster <5opkasuka@gmail.com>
2023-02-24 00:08:11 +0300

    upd

--- src/components/UseReducerHook.tsx
+++ src/components/UseReducerHook.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
-const UseReducerHook = (): JSX.Element => {
+enum EnumAction {
+  CHANGE = 'CHANGE',
+}
+
+interface Action {
+  type: EnumAction
+}
+
+interface State {
+  count: number
+  textState: boolean
+}
+
+
+const reducer = ( state: State, action: Action ) => {
+  switch (action.type) {
+    case EnumAction.CHANGE:
+      return {count: state.count + 1, textState: !state.textState}
+    default: return state
+  }
+}
+
+const UseReducerHook: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0, textState: false})
+  
   return (
-    <div>useReducerHook</div>
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({type: EnumAction.CHANGE})}>Increase</button>
+      {state.textState && <h1>Some text</h1>}
+    </div>
   )
 }
 
